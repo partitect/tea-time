84182d5f3eca0f8040651ad94d1930bcf81b81c2
Telat Kaya <telat@karatekin.edu.tr>
2020-06-19 12:01:38 +0300

    add one signal

--- js/dist/main.dev.js
+++ js/dist/main.dev.js
@@ -10,27 +10,11 @@ function selectedTea() {
   $('.caycilar').find('li').eq(selected).css("background-color", "green");
   var selectedUser = $('.caycilar').find('li').eq(selected).children('span').html();
   $(".selected").html(selectedUser);
-  notifyMe(selectedUser);
-} // request permission on page load
-
-
-document.addEventListener('DOMContentLoaded', function () {
-  if (!Notification) {
-    alert('Desktop notifications not available in your browser. Try Chromium.');
-    return;
-  }
-
-  if (Notification.permission !== 'granted') Notification.requestPermission();
-});
-
-function notifyMe(user) {
-  if (Notification.permission !== 'granted') Notification.requestPermission();else {
-    var notification = new Notification('Şanslı Kişimiz', {
-      icon: 'https://www.karatekin.edu.tr/img/logo_new.png',
-      body: user
-    });
-
-    notification.onclick = function () {//window.open('http://stackoverflow.com/a/13328397/1269037');
-    };
-  }
 }
+
+window.OneSignal = window.OneSignal || [];
+OneSignal.push(function () {
+  OneSignal.init({
+    appId: "e9918989-434e-45d7-bdf0-8ce3c8ca81dd"
+  });
+});
